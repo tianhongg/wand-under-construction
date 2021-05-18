bc8ada15da26de0cb629901193834e4e7b2df239
tianhongg <tianhongwang@Tianhongs-MacBook-Pro-2.local>
2021-05-18 14:27:51 -0400

    first

---
+++ mesh.h
@@ -0,0 +1,261 @@
+//----------------------------------------------------------------------------------||
+//-------------------                mesh.h                      -------------------||
+//----------------------------------------------------------------------------------||
+//                                                                                  ||
+//               __        ___    _   _ ____        ____ ___ ____                   ||
+//               \ \      / / \  | \ | |  _ \      |  _ \_ _/ ___|                  ||
+//                \ \ /\ / / _ \ |  \| | | | |_____| |_) | | |                      ||
+//                 \ V  V / ___ \| |\  | |_| |_____|  __/| | |___                   ||
+//                  \_/\_/_/   \_\_| \_|____/      |_|  |___\____|                  ||
+//                                                                                  ||
+//----------------------------------------------------------------------------------||
+//--  (W)akefield (A)cceleration a(n)d (D)LA - (P)article (i)n (C)ell Simulation  --||
+//----------------------------------------------------------------------------------||
+//---Author-----------           : Tianhong Wang                --------------------||
+//---Starting---------           : Jan-24-2019                  --------------------||
+//---Email------------           : tw474@cornell.edu            --------------------||
+//---Group------------           : Dr. Gennady Shvets' Group    --------------------||
+//---Copyright--------           : (C) 2019 by Tianhong Wang    --------------------||
+//----------------------------------------------------------------------------------||
+//----------------------------------------------------------------------------------||
+
+
+
+#ifndef H_MESH
+#define H_MESH
+
+#include <stdlib.h>
+
+class Detector;
+//---------------------------- Mesh class -----------------------
+class Mesh : public NList {
+
+   friend class Domain;
+
+private:
+   
+   Domain *p_domain() {return Domain::p_Domain;}; // pointer to domain class.
+
+   double dx, dy, dz, dt, dt0;
+   double dzz;
+
+
+   int GridX,GridY,GridZ; // Grid number in X, Y, Z;
+   int CellNum;           // Cell number in X-Y-Z domain;
+   int TrajNum;           // Traj number in X-Y transverse plane;
+
+   int RankIdx_X;
+   int RankIdx_Y;
+
+   int Rank;
+
+   double Offset_X;     //Defined as the Mesh Bottom_Left Corner.
+   double Offset_Y;     //Defined as the Mesh Bottom_Left Corner.
+
+//====plasma=======
+   int PProfileT;       //Plasma transverse profile
+   int PProfileL;       //Plasma longitudinal profile
+   int TpCellx;         //Trajectory per cell in x direction
+   int TpCelly;         //Trajectory per cell in y direction
+   int PushOrder;       //Push Trajectory Order in z direction
+   double AdaptiveStep;    //Adaptive Z-step for pushing trajectories
+   double V_thresh;
+   double Vmax;
+   double Vlim;
+
+//===Possible Ionization
+   int if_ioniz;      // if there is a need for ionization.
+   double Ion_R;
+   double Pla_ne;
+   double Dop_ne;
+   double Dop_TB;   //time begine
+   double Dop_TE;   //time end
+
+   dcomplex *dAx1;     //store the difference A(t+1)-A(t)
+   dcomplex *dAx2;     //store the difference A(t+1)-A(t)
+   dcomplex *dAy1;     //store the difference A(t+1)-A(t)
+   dcomplex *dAy2;     //store the difference A(t+1)-A(t)
+   double dA0;
+
+   Cell*        p_CellArray;
+   Trajectory*  p_Trajectory;
+   Particle*    p_Particle;
+   Detector*    XRayDetector;
+
+   double minGamma; // Smallest Gamma in domain
+   
+public:
+
+
+   double PlateauBegin;
+   double PlasmaBegin; 
+   double PlateauEnd; 
+   double PlasmaEnd; 
+   double PlasRadius;  
+
+
+   int GetRankIdx_X() {return RankIdx_X;};
+   int GetRankIdx_Y() {return RankIdx_Y;};
+
+   double GetOffset_X() {return Offset_X;};
+   double GetOffset_Y() {return Offset_Y;};
+
+   int GetPushOrder() {return PushOrder;};
+   
+   inline int GetCellN(int i, int j, int k)  //inline avoid function calling overhead.
+   {
+      return i+(GridX+2)*(j+(GridY+2)*k);
+   };
+
+   inline Cell& GetCell(int i, int j, int k)
+   {
+      return p_CellArray[GetCellN(i,j,k)];
+   };
+
+   inline Cell& GetCell(int n)
+   {
+       return p_CellArray[n];
+   };
+
+
+   //Get Physical Coordinates of the Cells in the Mesh;
+   inline double CellX(int i)
+   {
+      return dx*(i - 0.5)+Offset_X;
+   };
+
+   inline double CellY(int j)
+   {
+      return dy*(j - 0.5)+Offset_Y;
+   };
+
+   inline double CellZ(int k)
+   {
+      return dz*k;
+   };
+
+   void InitPulse(Pulse*);
+   void SetdAs();
+   void SeedTrajectory();
+   void SetIonDensity();
+   void SeedParticles(Specie *specie);
+   void ResetPlasma();
+   void AddTrajectory(Trajectory* p_Traj);
+   void   AddParticle(Particle*   p_Part);
+
+
+   void  MacroSource(int k);
+   void AdjustSource(int k);
+   void AdjustFields(int k);
+   void AdjustPsi(int k);
+   void SetSourceZero(int k);
+   void BeamSource();
+   void AdjustBSource();
+   void SetBSourceZero();
+   void SetFieldZeroAfter(int k);
+
+
+
+   void     Put_Chi(double k0, int k); 
+   void Partial_Psi(int k); 
+   void Pondermotive(int k);
+   void       Put_Jz(int k); 
+   void  LaserFields();
+   void  Ionization();  // initial stage simple version
+   int   Ifioniz(void) {return if_ioniz;};
+
+   double DopeBegin(void) {return Dop_TB;};
+   double DopeEnd(void)   {return Dop_TE;};
+
+   double  Dive_J(int i, int j, double k0, int k); // divergence of transverse current
+   double  Curl_J(int i, int j, double k0, int k); // Curl of transverse current
+   
+   double SourceX(int i, int j, double k0, int k);
+   double SourceY(int i, int j, double k0, int k);
+
+   int Get_TpCellx()
+   {
+      return TpCellx;
+   }
+
+   int Get_TpCelly()
+   {
+      return TpCelly;
+   }
+
+   double ProfileLongi(double xt, double yt, double zt);
+   double ProfileTrans(double xt, double yt, double zt);
+   void PushTrajectory(double k0, int k, int step);
+   void PushTrajectory_Half();
+   void PushTrajectory_HalfE(int k);
+   void PushTrajectory_HalfB(int k);
+   void PushParticle();
+   void SetNewTimeStep();
+   void AdjustZstep(double k0, int k, double &dz2dz);
+   void ExchangeT();
+   void ExchangeP();
+   void PackT(Trajectory* p_Traj, int Sendn, int where);
+   void PackP(Particle*   p_Part, int Sendn, int where);
+   Trajectory* Reconnect(Trajectory* p_Traj);
+   Particle*   Reconnect(Particle*   p_Part);
+   dcomplex SourceAx(int i, int j, int k, int NF);
+   dcomplex SourceAy(int i, int j, int k, int NF);
+   void Put_dA12(int what,  int k, int NF);
+   dcomplex GetdA0() {return dA0;}
+
+
+   Mesh(int XGridN, int YGridN, int ZGridN, FILE *f);
+   ~Mesh();
+};
+
+
+
+
+class Detector : public NList {
+
+   friend class Domain;
+   friend class Mesh;
+
+private:
+   
+   Domain *p_domain() {return Domain::p_Domain;}; // pointer to domain class.
+
+public:
+
+
+   double* p_DetectArray;
+   int IfRadiation;
+
+   
+   double ThetaMax;
+   int NTheta;
+
+   double PhiMax;
+   int NPhi;
+
+   double OmegaMin;
+   double OmegaMax;
+   int NOmega;  
+
+
+   // i=omega j=theta k=phi
+   inline int GetDectN(int i, int j, int k) 
+   {
+      return i+(NOmega)*(j+(NTheta)*k);
+   };
+
+   inline double GetDetector(int i, int j, int k)
+   {
+      return p_DetectArray[GetDectN(i,j,k)];
+   };
+
+   inline void PutPacket(int i, int j, int k, double EE)
+   {
+      p_DetectArray[GetDectN(i,j,k)]+=EE;
+      return;
+   };
+
+   Detector(FILE *f);
+   ~Detector();
+};
+#endif
---
+++ partition.cpp
@@ -0,0 +1,94 @@
+//----------------------------------------------------------------------------------||
+//-------------------                partition.cpp               -------------------||
+//----------------------------------------------------------------------------------||
+//                                                                                  ||
+//               __        ___    _   _ ____        ____ ___ ____                   ||
+//               \ \      / / \  | \ | |  _ \      |  _ \_ _/ ___|                  ||
+//                \ \ /\ / / _ \ |  \| | | | |_____| |_) | | |                      ||
+//                 \ V  V / ___ \| |\  | |_| |_____|  __/| | |___                   ||
+//                  \_/\_/_/   \_\_| \_|____/      |_|  |___\____|                  ||
+//                                                                                  ||
+//----------------------------------------------------------------------------------||
+//--  (W)akefield (A)cceleration a(n)d (D)LA - (P)article (i)n (C)ell Simulation  --||
+//----------------------------------------------------------------------------------||
+//---Author-----------           : Tianhong Wang                --------------------||
+//---Starting---------           : Jan-11-2019                  --------------------||
+//---Email------------           : tw474@cornell.edu            --------------------||
+//---Group------------           : Dr. Gennady Shvets' Group    --------------------||
+//---Copyright--------           : (C) 2019 by Tianhong Wang    --------------------||
+//----------------------------------------------------------------------------------||
+//----------------------------------------------------------------------------------||
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "wand_PIC.h"
+
+
+//---------------------------- Partition::Partition -----------------------
+Partition::Partition (FILE *f) : NList ("Partition")
+{
+  
+  MPI_Comm_rank(MPI_COMM_WORLD, &Rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &N_Processor);
+
+
+  AddEntry((char*)"Xpartition", &N_Xpart, 1);
+  AddEntry((char*)"Ypartition", &N_Ypart, 1);
+
+  if (f)
+    {
+      rewind(f);
+      read(f);
+    }
+
+  if (N_Processor != N_Xpart*N_Ypart)
+    {
+      if(Rank==0) std::cout<< "Error Partition Number. \n";
+      exit(-10);
+    }
+
+// coordinates of the processor
+  idx_Y= (Rank/N_Xpart)+1;
+  idx_X= (Rank%N_Xpart)+1;  //tianhong May-18-2021
+
+//Neighbor Processors: m==minus, p=plus.
+
+  idx_XmPE=Rank-1;
+  idx_XpPE=Rank+1;
+
+
+ //tianhong May-18-2021
+  if(idx_X==1)idx_XmPE+=N_Xpart; 
+  if(idx_X==N_Xpart) idx_XpPE-=N_Xpart;
+
+  idx_YmPE=Rank-N_Xpart;
+  idx_YpPE=Rank+N_Xpart;
+
+ //tianhong May-18-2021
+  if(idx_Y==1) idx_YmPE+=N_Xpart*(N_Ypart);
+  if(idx_Y==N_Ypart) idx_YpPE-=N_Xpart*(N_Ypart);
+
+
+  idx_mmPE=idx_YmPE-1;
+  idx_pmPE=idx_YmPE+1;
+
+ //tianhong May-18-2021
+  if(idx_X==1)
+  {
+    idx_mmPE+=N_Xpart;
+    idx_mpPE+=N_Xpart; 
+  }
+  if(idx_X==N_Xpart)
+  {
+    idx_pmPE-=N_Xpart;
+    idx_ppPE-=N_Xpart; 
+  }
+  
+  idx_mpPE=idx_YpPE-1;
+  idx_ppPE=idx_YpPE+1;
+
+  // if(idx_Y==1) idx_pmPE+=N_Xpart*(N_Ypart);
+  // if(idx_Y==N_Ypart) idx_ppPE-=N_Xpart*(N_Ypart);
+  
+
+}
